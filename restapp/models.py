5dea62adbe0ef10e51017c422d100344e3f16a85
ankit singh <ankitsinghtheweeknd@gmail.com>
2023-12-07 15:03:42 +0545

    hyperlinkedcommit

--- restapp/models.py
+++ restapp/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Journalist(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     author = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
